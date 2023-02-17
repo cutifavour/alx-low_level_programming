6070f488d17fd27478f5a5fcc6ed6e11c9aee07d
Izo Chiemeke <cuti.favour@yahoo.com>
2023-02-16 18:57:26 -0800

    hello world project

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * return: always 0
+ */
+{ printf("Size of a char: %i byte(s)\n", sizeof(char));
+printf("Size of a int: %i byte(s)\n", sizeof(init));
+printf("Size of a long: %i byte(s)\n", sizeof(long));
+printf("Size of a float: %i byte(s)\n", sizeof(char));
+return (0); }
