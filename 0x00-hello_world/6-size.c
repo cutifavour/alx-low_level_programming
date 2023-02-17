ccae5192b6d4e5f2f2b16f7f5d2de6d4b1ec8084
Izo Chiemeke <cuti.favour@yahoo.com>
2023-02-16 19:35:01 -0800

    hello world project

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - entry
- * return: always 0
+ * main - a program that prints size of various types
+ * Return: always 0
  */
-{ printf("Size of a char: %i byte(s)\n", sizeof(char));
-printf("Size of a int: %i byte(s)\n", sizeof(init));
-printf("Size of a long: %i byte(s)\n", sizeof(long));
-printf("Size of a float: %i byte(s)\n", sizeof(char));
+int main(void)
+{ printf("Size of a char: %lu  byte(s)\n", (unsigned long) sizeof(char));
+printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(int));
+printf("Size of a long: %lu byte(s)\n", (unsigned long)sizeof(long));
+printf("Size of a float: %lu  byte(s)\n", (unsigned long)sizeof(char));
 return (0); }
