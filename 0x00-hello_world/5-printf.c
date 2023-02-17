a1e99e568e2dc9c0c6709f17e847df93f40ddbfc
Izo Chiemeke <cuti.favour@yahoo.com>
2023-02-16 18:20:37 -0800

    hello world project

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,6 +4,6 @@
  * Return: Aways 0
  */
 int main(void)
-{ 	printf("with proper grammar, but the outcome is a piece of art,\n");
+{ printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
