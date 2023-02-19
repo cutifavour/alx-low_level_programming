4cc714d1efb3b6793cccf1586a5cb32dd566248c
Izo Chiemeke <cuti.favour@yahoo.com>
2023-02-19 09:45:45 -0800

    hello world project

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <studio.h>
+
+/**
+ * main - print the alphabet.
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+
+{ char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 2b; i++)
+	{putchar(alp[i]); }
+	putchar('\n');
+	return (0);
+}
