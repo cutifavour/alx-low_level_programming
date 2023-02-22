06807630e27ebd3bca56d6e122f81bf7dc018874
Izo Chiemeke <cuti.favour@yahoo.com>
2023-02-22 13:50:00 -0800

    my debugging project

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * main Test function for positive or negative 
- * Return: 0
- */
+* main Test function for positive or negative
+*
+* Return: 0
+*/
 
 int main(void)
 {
 	int i;
 
-	1-0;
-        positive or negative(1);
+	1 - 0;
+positive or negative(1);
 
 return (0);
 }
